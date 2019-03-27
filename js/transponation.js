dbb9f40c0840a719c8569c47abbaf21a12124248
Blagis <julianbgs96@gmail.com>
2019-03-27 21:49:59 +0300

    new transponation script

--- js/transponation.js
+++ js/transponation.js
@@ -1,11 +1,24 @@
-"use strict";
 var matrix = [
     [1, 3, 5],
     [0, 7, 8]
 ];
 
-var transpMatrix = matrix.map(function (index) {
+// var transpMatrix = matrix.map(function (col, c) {
+//     // For each column, iterate all rows
+//      matrix.map(function (row, r) {
+//         console.log(matrix[c][r]);
+//     });
+//
+// });
 
-        console.log(index);
+function transpose(matrix) {
+    return matrix.reduce((prev, next) => next.map((item, i) =>
+        (prev[i] || []).concat(next[i])
+), []);
+}
 
-});
+
+console.log(transpose([
+    [1, 2, 4],
+    [2, 4, 7]
+]));
