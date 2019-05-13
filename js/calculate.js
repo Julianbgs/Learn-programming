e39db840e6a3f9ef9831ba42464e88fc07c19490
Julian <julianbgs96@gmail.com>
2019-05-13 17:33:12 +0300

    calculate new code

--- js/calculate.js
+++ js/calculate.js
@@ -1,25 +1,192 @@
 "use strict";
-let select = document.querySelector('#typeCar');
+// data easy car
+let typeMotor = document.querySelector('#typeMotor');
+let volume = document.querySelector('#volume');
+let currency = document.querySelector('#currency');
+let country = document.querySelector('#place');
 let btn = document.querySelector('#btn');
 let result = document.querySelector('#result');
-let coupe = 150000;
-let sedan = 200000;
-let offroad = 250000;
-let calc = 0;
+// end data easy car
 
-btn.addEventListener('click', function () {
-        let selectVal = select.value;
-        console.log(selectVal);
-        if(selectVal === 'Coupe'){
-           calc = coupe;
-           result.innerHTML = `<p>Общая сумма :</p>`+ " " + calc + ".";
+//data electro car
+let countryElectro = document.querySelector('#placeElectro');
+let motorElectro = document.querySelector('#typeMotorElectro');
+let volumeElectro = document.querySelector('#volumeElectro');
+let currencyElectro = document.querySelector('#currencyElectro');
+let electroBtn = document.querySelector('#btnElectro');
+let resultElectro = document.querySelector('#resultElectro');
+//end data electro car
+
+let openModal = document.querySelector('#openModal');
+let closeModal = document.querySelector('#closeModal');
+let modal = document.querySelector('#modal');
+// count data
+let calc = 0;
+let calcUsd = 0;
+// end count data
+
+//tabs
+function Tabs() {
+    let bindAll = function() {
+        let menuElements = document.querySelectorAll('[data-tab]');
+        for(let i = 0; i < menuElements.length ; i++) {
+            menuElements[i].addEventListener('click', change, false);
         }
-        if(selectVal === 'Sedan'){
-            calc = sedan;
-            result.innerHTML = `<p>Общая сумма :</p>`+ " " + calc + ".";
+    };
+
+    let clear = function() {
+        let menuElements = document.querySelectorAll('[data-tab]');
+        for(let i = 0; i < menuElements.length ; i++) {
+            menuElements[i].classList.remove('active');
+            let id = menuElements[i].getAttribute('data-tab');
+            document.getElementById(id).classList.remove('active');
         }
-        if(selectVal === 'Offroad'){
-            calc = offroad;
-            result.innerHTML = `<p>Общая сумма :</p>`+ " " + calc + ".";
+    };
+
+    let change = function(e) {
+        clear();
+        e.target.classList.add('active');
+        let id = e.currentTarget.getAttribute('data-tab');
+        document.getElementById(id).classList.add('active');
+    };
+
+    bindAll();
+}
+
+let connectTabs = new Tabs();
+//end tabs
+
+openModal.addEventListener('click', function () {
+    modal.style.display = 'block';
+
+    closeModal.addEventListener('click', function () {
+       modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (e) {
+        if(e.target === modal){
+            modal.style.display = 'none';
         }
+    })
 });
+// calc easy car
+btn.addEventListener('click', function () {
+    let countryVal = country.value;
+
+    if (countryVal === 'usa'){
+        calc = 100000;
+        calcUsd = 1800;
+    }
+    if (countryVal === 'europe') {
+        calc = 120000;
+        calcUsd = 2000;
+    }
+    if (countryVal === 'russia') {
+        calc = 60000;
+        calcUsd = 1000;
+    }
+
+    let motorVal = typeMotor.value;
+    if (motorVal === 'dizel') {
+        calc += 20000;
+        calcUsd += 350;
+    }
+    if (motorVal === 'benz') {
+        calc += 18000;
+        calcUsd += 320;
+    }
+    if (motorVal === 'gaz') {
+        calc += 15000;
+        calcUsd += 300;
+    }
+
+    let volumeVal = volume.value;
+
+    if (volumeVal === '1200') {
+        calc += 10000;
+        calcUsd += 180;
+    }
+    if (volumeVal === '1999') {
+        calc += 13000;
+        calcUsd += 210;
+    }
+    if (volumeVal === '2400') {
+        calc += 17000;
+        calcUsd += 290;
+    }
+    if (volumeVal === '2900') {
+        calc += 20000;
+        calcUsd += 370;
+    }
+
+    let curVal = currency.value;
+
+    if ( curVal === 'rub') {
+        result.innerHTML = `<p>Общая сумма :</p>` + " " + calc + "rub.";
+    }
+    if( curVal === 'usd') {
+        result.innerHTML = `<p>Общая сумма :</p>` + " " + calcUsd + "usd.";
+    }
+});
+//end calc
+
+//calc electro car
+electroBtn.addEventListener('click', function () {
+    let countryElVal = countryElectro.value;
+
+    if (countryElVal === 'usa'){
+        calc = 100000;
+        calcUsd = 1800;
+    }
+    if (countryElVal === 'europe') {
+        calc = 120000;
+        calcUsd = 2000;
+    }
+    if (countryElVal === 'russia') {
+        calc = 60000;
+        calcUsd = 1000;
+    }
+
+    let motorElVal = motorElectro.value;
+    if (motorElVal === 'dizel') {
+        calc += 20000;
+        calcUsd += 350;
+    }
+    if (motorElVal === 'benz') {
+        calc += 18000;
+        calcUsd += 320;
+    }
+    if (motorElVal === 'gaz') {
+        calc += 15000;
+        calcUsd += 300;
+    }
+
+    let volumeElVal = volumeElectro.value;
+
+    if (volumeElVal === '1200') {
+        calc += 10000;
+        calcUsd += 180;
+    }
+    if (volumeElVal === '1999') {
+        calc += 13000;
+        calcUsd += 210;
+    }
+    if (volumeElVal === '2400') {
+        calc += 17000;
+        calcUsd += 290;
+    }
+    if (volumeElVal === '2900') {
+        calc += 20000;
+        calcUsd += 370;
+    }
+
+    let curElVal = currencyElectro.value;
+
+    if ( curElVal === 'rub') {
+        resultElectro.innerHTML = `<p>Общая сумма :</p>` + " " + calc + "rub.";
+    }
+    if( curElVal === 'usd') {
+        resultElectro.innerHTML = `<p>Общая сумма :</p>` + " " + calcUsd + "usd.";
+    }
+});
+//end calc
